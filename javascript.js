b8b6eb76d33e28c20a2d505d8fd175c7d93fb2ec
SamBillingsley <sambillingsleywork@gmail.com>
2023-03-20 14:43:40 -0500

    Add buttons with event listeners
    three buttons for each player choice

--- javascript.js
+++ javascript.js
@@ -1,37 +1,44 @@
 // Make array for each choice
 const choices = ["rock", "paper", "scissors"];
-function randNum() {
-  return Math.floor(Math.random() * 3);
-}
 
 // Make a function for the computers choice, which chooses a random string from the choices array
-function getComputerChoice() {
-  return choices[randNum()];
+function computerChoice() {
+  let randNum = Math.floor(Math.random() * 3);
+  return choices[randNum];
 }
-
-let computerChoice = getComputerChoice();
 
 // Ask the user to choose one of the three choices
-function getPlayerChoice() {
-  let userInput = prompt("Rock, Paper, or Scissors?");
-  const input = userInput.toLowerCase;
-  if (userInput.toLowerCase() === "rock") {
-    return choices[0];
-  } else if (userInput.toLowerCase() === "paper") {
-    return choices[1];
-  } else if (userInput.toLowerCase() === "scissors") {
-    return choices[2];
-  } else {
-    alert("Not a valid response, try again.");
-    getPlayerChoice();
-  }
-}
+// function getPlayerChoice() {
+//   let userInput = prompt("Rock, Paper, or Scissors?");
+//   const input = userInput.toLowerCase;
+//   if (userInput.toLowerCase() === "rock") {
+//     return choices[0];
+//   } else if (userInput.toLowerCase() === "paper") {
+//     return choices[1];
+//   } else if (userInput.toLowerCase() === "scissors") {
+//     return choices[2];
+//   } else {
+//     alert("Not a valid response, try again.");
+//     getPlayerChoice();
+//   }
+// }
 
-let playerChoice = getPlayerChoice();
+// let playerChoice = getPlayerChoice();
 
 // Create playerWins and computerWins variable to keep track of wins
 let playerWins = 0;
 let computerWins = 0;
+
+// Create event listeners for buttons to select playerChoice
+const rockBtn = document.getElementById("rock");
+const paperBtn = document.getElementById("paper");
+const scissorsBtn = document.getElementById("scissors");
+
+rockBtn.addEventListener("click", () => playRound("rock", computerChoice()));
+paperBtn.addEventListener("click", () => playRound("paper", computerChoice()));
+scissorsBtn.addEventListener("click", () =>
+  playRound("scissors", computerChoice())
+);
 
 // Create a function to decide the winner
 function playRound(playerChoice, computerChoice) {
@@ -54,27 +61,27 @@ function playRound(playerChoice, computerChoice) {
   }
 }
 
-// Create a function that displays the score
-function score(playerWins, computerWins) {
-  console.log(`Player: ${playerWins}`);
-  console.log(`Computer: ${computerWins}`);
-}
+// // Create a function that displays the score
+// function score(playerWins, computerWins) {
+//   console.log(`Player: ${playerWins}`);
+//   console.log(`Computer: ${computerWins}`);
+// }
 
-// Create game function, best of 5
-function game(playerChoice, computerChoice) {
-  playRound(playerChoice, computerChoice);
-  score(playerWins, computerWins);
-  while (playerWins < 3 && computerWins < 3) {
-    playerChoice = getPlayerChoice();
-    computerChoice = getComputerChoice();
-    playRound(playerChoice, computerChoice);
-    score(playerWins, computerWins);
-  }
-  if (playerWins === 3) {
-    console.log("Player has won the best of 5!");
-  } else {
-    console.log("Computer has won the best of 5! You lose.");
-  }
-}
+// // Create game function, best of 5
+// function game(playerChoice, computerChoice) {
+//   playRound(playerChoice, computerChoice);
+//   score(playerWins, computerWins);
+//   while (playerWins < 3 && computerWins < 3) {
+//     playerChoice = getPlayerChoice();
+//     computerChoice = getComputerChoice();
+//     playRound(playerChoice, computerChoice);
+//     score(playerWins, computerWins);
+//   }
+//   if (playerWins === 3) {
+//     console.log("Player has won the best of 5!");
+//   } else {
+//     console.log("Computer has won the best of 5! You lose.");
+//   }
+// }
 
-game(playerChoice, computerChoice);
+// playRound(playerChoice, computerChoice);
